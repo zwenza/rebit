dea82180a94dca5ed42947be0cc6f7d1a849921c
David Jöch <david.joech@catalysts.cc>
2016-12-11 16:17:35 +0100

    added react-router

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import App from './containers/App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import { Router, Route, browserHistory } from 'react-router'
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
