a81c99d53b6ac34ccd3b47cde3253f7eccd0bf99
David Jöch <david.joech@catalysts.cc>
2016-12-20 18:24:15 +0100

    persist redux-store in local-storage now

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import store from './store'
 import { Provider } from 'react-redux'
-import { Router, Route, IndexRoute, browserHistory   } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import Dashboard from './components/Dashboard'
 import Auth from './containers/Auth'
 import Home from './components/Home'
 import _ from 'lodash'
+
 import 'antd/dist/antd.css'
 
 const requireAuthentication = (nextState, transition) => {
--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import { createEpicMiddleware } from 'redux-observable'
 import rootReducer from './reducers'
 import rootEpic from './epics'
+import persistState from 'redux-localstorage'
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(rootReducer, composeEnhancers(
-  applyMiddleware(epicMiddleware)
+  applyMiddleware(epicMiddleware),
+  persistState(), // redux-localstorage
 ));
