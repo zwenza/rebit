a4980af898a792660649e5ecbb32eb5ea28206ee
David Jöch <david.joech@catalysts.cc>
2016-12-23 18:32:21 +0100

    intraday-data setup

--- src/actions/data.js
+++ src/actions/data.js
@@ -1,4 +1,5 @@
 import * as types from '../constants/ActionTypes';
 
 export const fetchHeartRateData = timeFrame => ({ type: types.GET_HEART_RATE, payload: { timeFrame } })
+export const fetchHeartRateIntraDayData = timeFrame => ({ type: types.GET_HEART_RATE_INTRADAY, payload: { timeFrame } })
 export const setDataTimeFrame = dataTimeFrame => ({ type: types.SET_DATA_TIME_FRAME, payload: { dataTimeFrame }})
--- src/epics/data.js
+++ src/epics/data.js
@@ -5,7 +5,37 @@ import 'rxjs';
 export const getHeartrate = (action$, store) => action$
   .ofType(ActionTypes.GET_HEART_RATE)
   .switchMap(data => Observable.ajax({
-      url: 'https://api.fitbit.com/1/user/' +  store.getState().auth.user_id + '/activities/heart/date/2016-12-01/' + data.payload.timeFrame + '.json',
+      url: 'https://api.fitbit.com/1/user/' +  store.getState().auth.user_id + '/activities/heart/date/' + data.payload.timeFrame[0] + '/' + data.payload.timeFrame[1] + '.json',
+      headers: {
+        'Authorization': 'Bearer ' + store.getState().auth.access_token
+      },
+      crossDomain: true,
+      method: 'GET',
+      createXHR: function () {
+        return new XMLHttpRequest();
+      }
+  }).map(res => res.response)
+    .mergeMap(response => Observable.of(
+      {
+        type: ActionTypes.GET_HEART_RATE_SUCCESS,
+        payload: response,
+      }
+    ))
+    .catch(error => Observable.of(
+      {
+        type: ActionTypes.GET_HEART_RATE_FAILURE,
+        payload: {
+          error,
+        },
+      }
+    )),
+  );
+
+
+export const getHeartrateIntraDay = (action$, store) => action$
+  .ofType(ActionTypes.GET_HEART_RATE_INTRADAY)
+  .switchMap(data => Observable.ajax({
+      url: 'https://api.fitbit.com/1/user/' +  store.getState().auth.user_id + '/activities/heart/date/' + data.payload.timeFrame[0] + '/' + data.payload.timeFrame[1] + '/1min' + '.json',
       headers: {
         'Authorization': 'Bearer ' + store.getState().auth.access_token
       },
